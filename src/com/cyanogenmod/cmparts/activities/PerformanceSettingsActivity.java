a8d32bbe611b8d6236d44a06072546ed541a07e6
NameLess, the Jedi <yoddah@gmail.com>
2010-10-04 11:38:32 +0200

    Add Swap On/Off switch

--- src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
+++ src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
@@ -26,6 +26,12 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
 
     private static final String COMPCACHE_DEFAULT = "25";
 
+    private static final String SWAP_PERSIST_PROP = "persist.sys.swap";
+
+    private static final String SWAP_PREF = "pref_swap";
+
+    private static final String SWAP_DEFAULT = "0";
+
     private static final String JIT_PREF = "pref_jit_mode";
     
     private static final String JIT_ENABLED = "int:jit";
@@ -60,6 +66,8 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
 
     private ListPreference mCompcachePref;
 
+    private CheckBoxPreference mSwapPref;
+
     private CheckBoxPreference mJitPref;
 
     private CheckBoxPreference mUseDitheringPref;
@@ -84,11 +92,16 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
         PreferenceScreen prefSet = getPreferenceScreen();
         
         mCompcachePref = (ListPreference) prefSet.findPreference(COMPCACHE_PREF);
+        mSwapPref = (CheckBoxPreference) prefSet.findPreference(SWAP_PREF);
         if (isSwapAvailable()) {
             mCompcachePref.setValue(SystemProperties.get(COMPCACHE_PERSIST_PROP, COMPCACHE_DEFAULT));
             mCompcachePref.setOnPreferenceChangeListener(this);
+
+            String useSwap = SystemProperties.get(SWAP_PERSIST_PROP, SWAP_DEFAULT);
+            mSwapPref.setChecked("1".equals(useSwap));
         } else {
             prefSet.removePreference(mCompcachePref);
+            prefSet.removePreference(mSwapPref);
         }
 
         mJitPref = (CheckBoxPreference) prefSet.findPreference(JIT_PREF);
@@ -128,6 +141,19 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
     
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mSwapPref) {
+            boolean checked = mSwapPref.isChecked();
+            if (checked) {
+                SystemProperties.set(SWAP_PERSIST_PROP, "1");
+                SystemProperties.set(COMPCACHE_PERSIST_PROP, "0");
+                mCompcachePref.setValue("0");
+            } else {
+                SystemProperties.set(SWAP_PERSIST_PROP, "0");
+                SystemProperties.set(COMPCACHE_PERSIST_PROP, COMPCACHE_DEFAULT);
+                mCompcachePref.setValue(COMPCACHE_DEFAULT);
+            }
+            return true;
+        }
         if (preference == mJitPref) {
             SystemProperties.set(JIT_PERSIST_PROP, 
                     mJitPref.isChecked() ? JIT_ENABLED : JIT_DISABLED);
@@ -165,6 +191,11 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
         if (preference == mCompcachePref) {
             if (newValue != null) {
                 SystemProperties.set(COMPCACHE_PERSIST_PROP, (String)newValue);
+                if ( ! newValue.equals("0") ) {
+                    // CompCache is enabled - disable Swap
+                    SystemProperties.set(SWAP_PERSIST_PROP, "0");
+                    mSwapPref.setChecked(false);
+                }
                 return true;
             }
         }
