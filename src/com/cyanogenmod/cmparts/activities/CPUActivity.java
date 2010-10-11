ee565d35147c64449a23068bcdd68640d0e13364
NameLess, the Jedi <yoddah@gmail.com>
2010-10-11 12:38:24 +0200

    Fixing the glitches

--- src/com/cyanogenmod/cmparts/activities/CPUActivity.java
+++ src/com/cyanogenmod/cmparts/activities/CPUActivity.java
@@ -2,16 +2,17 @@ package com.cyanogenmod.cmparts.activities;
 
 import com.cyanogenmod.cmparts.R;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+// import android.app.AlertDialog;
+// import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.SystemProperties;
-import android.preference.CheckBoxPreference;
+// import android.os.SystemProperties;
+// import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
-import android.provider.Settings;
+// import android.provider.Settings;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -35,6 +36,9 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
     private ListPreference GovPref;
     private ListPreference MinFreqPref;
     private ListPreference MaxFreqPref;
+    private Preference GovSel;
+    private Preference FreqMin;
+    private Preference FreqMax;
 
 
     @Override
@@ -44,52 +48,58 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
         //
         // Read data from kernel
         //
-        private String[] Governors = ReadOneLine(GOVERNORS_LIST_FILE).split(" ");
-        private String[] Freqs = ReadOneLine(FREQ_LIST_FILE).split(" ");
-        private String MinFreq = ReadOneLine(FREQ_MIN_FILE);
-        private String MaxFreq = ReadOneLine(FREQ_MAX_FILE);
-        private String Gov = ReadOneLine(GOVERNOR);
+        String[] Governors = ReadOneLine(GOVERNORS_LIST_FILE).split(" ");
+        String[] Freqs = ReadOneLine(FREQ_LIST_FILE).split(" ");
+        String MinFreq = ReadOneLine(FREQ_MIN_FILE);
+        String MaxFreq = ReadOneLine(FREQ_MAX_FILE);
+        String Gov = ReadOneLine(GOVERNOR);
 
         //
         // UI
         //
-        setTitle(R.string.cpu_settings_title);
+        setTitle("CPU Settings");
         addPreferencesFromResource(R.xml.cpu_settings);
         
         PreferenceScreen PrefScreen = getPreferenceScreen();
 
-        setStringSummary("gov_selected", Gov);
+        GovSel = PrefScreen.findPreference("gov_selected");
+        GovSel.setSummary(Gov);
+
         GovPref = (ListPreference) PrefScreen.findPreference(GOV_PREF);
         GovPref.setEntryValues(Governors);
         GovPref.setEntries(Governors);
 
-        setStringSummary("freq_min", MinFreq);
+        FreqMin = PrefScreen.findPreference("freq_min");
+        FreqMin.setSummary(MinFreq);
+
         MinFreqPref = (ListPreference) PrefScreen.findPreference(MIN_FREQ_PREF);
         MinFreqPref.setEntryValues(Freqs);
         MinFreqPref.setEntries(Freqs);
 
-        setStringSummary("freq_max", MaxFreq);
+        FreqMax = PrefScreen.findPreference("freq_max");
+        FreqMax.setSummary(MaxFreq);
+
         MaxFreqPref = (ListPreference) PrefScreen.findPreference(MAX_FREQ_PREF);
         MaxFreqPref.setEntryValues(Freqs);
-        MaxFreqPref.setEntires(Freqs);
+        MaxFreqPref.setEntries(Freqs);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == GovPref) {
             if (newValue != null) {
-                setStringSummary("gov_selected", (String) newValue);
+                GovSel.setSummary((String) newValue);
                 return true;
             }
         }
         if (preference == MinFreqPref) {
             if (newValue != null) {
-                setStringSummary("freq_min", (String) newValue);
+                FreqMin.setSummary((String) newValue);
                 return true;
             }
         }
         if (preference == MaxFreqPref) {
             if (newValue != null) {
-                setStringSummary("freq_max", (String) newValue);
+                FreqMax.setSummary((String) newValue);
                 return true;
             }
         }
