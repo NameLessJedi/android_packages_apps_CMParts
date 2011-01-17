99bb30397dc50f280e78b3e9160f23d0ab9d6b97
NameLess, the Jedi <yoddah@gmail.com>
2011-01-17 11:16:20 +0100

    CPU Settings now restored after (re)boot. Restoring is done after SDCard is mounted so: 1. if there is no SDCard - it won't be restored (need to fix this probably) 2. on my phone SDCard gets mounted around 2-3 mins after reboot - this    gives user narrow time frame to change settings in case OC causes    probs. I realize this is not ideal solution. Please bear with me for    now.

--- src/com/cyanogenmod/cmparts/activities/CPUActivity.java
+++ src/com/cyanogenmod/cmparts/activities/CPUActivity.java
@@ -11,8 +11,8 @@ import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.Process;
 
@@ -21,23 +21,24 @@ import java.lang.Process;
 //
 public class CPUActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
-    private static final String GOV_PREF = "pref_cpu_gov";
-    private static final String GOVERNORS_LIST_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors";
-    private static final String GOVERNOR = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor";
+    public static final String GOV_PREF = "pref_cpu_gov";
+    public static final String GOVERNORS_LIST_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors";
+    public static final String GOVERNOR = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor";
 
-    private static final String MIN_FREQ_PREF = "pref_freq_min";
-    private static final String MAX_FREQ_PREF = "pref_freq_max";
-    private static final String FREQ_LIST_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies";
-    private static final String FREQ_MAX_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq";
-    private static final String FREQ_MIN_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq";
-    private static final String LOGTAG = "CPUSettings";
+    public static final String MIN_FREQ_PREF = "pref_freq_min";
+    public static final String MAX_FREQ_PREF = "pref_freq_max";
+    public static final String FREQ_LIST_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies";
+    public static final String FREQ_MAX_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq";
+    public static final String FREQ_MIN_FILE = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq";
+    public static final String SOB_PREF = "pref_set_on_boot";
+    private static final String TAG = "CPUSettings";
     private String GOV_FMT;
     private String MIN_FMT;
     private String MAX_FMT;
 
-    private ListPreference GovPref;
-    private ListPreference MinFreqPref;
-    private ListPreference MaxFreqPref;
+    private ListPreference govPref;
+    private ListPreference minFreqPref;
+    private ListPreference maxFreqPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +48,8 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
         MIN_FMT = getString(R.string.cpu_min_freq);
         MAX_FMT = getString(R.string.cpu_max_freq);
 
-        String[] Governors = ReadOneLine(GOVERNORS_LIST_FILE).split(" ");
-        String[] FreqValues = ReadOneLine(FREQ_LIST_FILE).split(" ");
+        String[] Governors = readOneLine(GOVERNORS_LIST_FILE).split(" ");
+        String[] FreqValues = readOneLine(FREQ_LIST_FILE).split(" ");
         String[] Freqs;
         String temp;
 
@@ -57,40 +58,37 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
             Freqs[i] = MHerzed(FreqValues[i]);
         }
 
-        //
-        // UI
-        //
         setTitle(R.string.cpu_title);
         addPreferencesFromResource(R.xml.cpu_settings);
-        
+
         PreferenceScreen PrefScreen = getPreferenceScreen();
 
-        temp = ReadOneLine(GOVERNOR);
+        temp = readOneLine(GOVERNOR);
 
-        GovPref = (ListPreference) PrefScreen.findPreference(GOV_PREF);
-        GovPref.setEntryValues(Governors);
-        GovPref.setEntries(Governors);
-        GovPref.setValue(temp);
-        GovPref.setSummary(String.format(GOV_FMT, temp));
-        GovPref.setOnPreferenceChangeListener(this);
+        govPref = (ListPreference) PrefScreen.findPreference(GOV_PREF);
+        govPref.setEntryValues(Governors);
+        govPref.setEntries(Governors);
+        govPref.setValue(temp);
+        govPref.setSummary(String.format(GOV_FMT, temp));
+        govPref.setOnPreferenceChangeListener(this);
 
-        temp = ReadOneLine(FREQ_MIN_FILE);
+        temp = readOneLine(FREQ_MIN_FILE);
 
-        MinFreqPref = (ListPreference) PrefScreen.findPreference(MIN_FREQ_PREF);
-        MinFreqPref.setEntryValues(FreqValues);
-        MinFreqPref.setEntries(Freqs);
-        MinFreqPref.setValue(temp);
-        MinFreqPref.setSummary(String.format(MIN_FMT, MHerzed(temp)));
-        MinFreqPref.setOnPreferenceChangeListener(this);
+        minFreqPref = (ListPreference) PrefScreen.findPreference(MIN_FREQ_PREF);
+        minFreqPref.setEntryValues(FreqValues);
+        minFreqPref.setEntries(Freqs);
+        minFreqPref.setValue(temp);
+        minFreqPref.setSummary(String.format(MIN_FMT, MHerzed(temp)));
+        minFreqPref.setOnPreferenceChangeListener(this);
 
-        temp = ReadOneLine(FREQ_MAX_FILE);
+        temp = readOneLine(FREQ_MAX_FILE);
 
-        MaxFreqPref = (ListPreference) PrefScreen.findPreference(MAX_FREQ_PREF);
-        MaxFreqPref.setEntryValues(FreqValues);
-        MaxFreqPref.setEntries(Freqs);
-        MaxFreqPref.setValue(temp);
-        MaxFreqPref.setSummary(String.format(MAX_FMT, MHerzed(temp)));
-        MaxFreqPref.setOnPreferenceChangeListener(this);
+        maxFreqPref = (ListPreference) PrefScreen.findPreference(MAX_FREQ_PREF);
+        maxFreqPref.setEntryValues(FreqValues);
+        maxFreqPref.setEntries(Freqs);
+        maxFreqPref.setValue(temp);
+        maxFreqPref.setSummary(String.format(MAX_FMT, MHerzed(temp)));
+        maxFreqPref.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -99,50 +97,47 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
 
         super.onResume();
 
-        temp = ReadOneLine(FREQ_MAX_FILE);
-        MaxFreqPref.setValue(temp);
-        MaxFreqPref.setSummary(String.format(MAX_FMT, MHerzed(temp)));
+        temp = readOneLine(FREQ_MAX_FILE);
+        maxFreqPref.setValue(temp);
+        maxFreqPref.setSummary(String.format(MAX_FMT, MHerzed(temp)));
 
-        temp = ReadOneLine(FREQ_MIN_FILE);
-        MinFreqPref.setValue(temp);
-        MinFreqPref.setSummary(String.format(MIN_FMT, MHerzed(temp)));
+        temp = readOneLine(FREQ_MIN_FILE);
+        minFreqPref.setValue(temp);
+        minFreqPref.setSummary(String.format(MIN_FMT, MHerzed(temp)));
 
-        temp = ReadOneLine(GOVERNOR);
-        GovPref.setSummary(String.format(GOV_FMT, temp));
+        temp = readOneLine(GOVERNOR);
+        govPref.setSummary(String.format(GOV_FMT, temp));
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-        String rootcmd = null;
-        String filename;
+        String fname = "";
 
         if (newValue != null) {
-            if (preference == GovPref) {
-                rootcmd = "echo " + (String) newValue + " > " + GOVERNOR + "\n";
-            } else if (preference == MinFreqPref) {
-                rootcmd = "echo " + (String) newValue + " > " + FREQ_MIN_FILE + "\n";
-            } else if (preference == MaxFreqPref) {
-                rootcmd = "echo " + (String) newValue + " > " + FREQ_MAX_FILE + "\n";
+            if (preference == govPref) {
+                fname = GOVERNOR;
+            } else if (preference == minFreqPref) {
+                fname = FREQ_MIN_FILE;
+            } else if (preference == maxFreqPref) {
+                fname = FREQ_MAX_FILE;
             }
-            try {
-                RootInvoker(rootcmd);
-                if (preference == GovPref) {
-                    GovPref.setSummary(String.format(GOV_FMT, (String) newValue));
-                } else if (preference == MinFreqPref) {
-                    MinFreqPref.setSummary(String.format(MIN_FMT, MHerzed((String) newValue)));
-                } else if (preference == MaxFreqPref) {
-                    MaxFreqPref.setSummary(String.format(MAX_FMT, MHerzed((String) newValue)));
-                }
-            } catch (IOException e) {
-                Log.e(LOGTAG, "RootInvoker Exception", e);
-            }
-            return true;
-        } else {
-            return false;
+
+           if (writeOneLine(fname, (String) newValue)) {
+               if (preference == govPref) {
+                   govPref.setSummary(String.format(GOV_FMT, (String) newValue));
+               } else if (preference == minFreqPref) {
+                   minFreqPref.setSummary(String.format(MIN_FMT, MHerzed((String) newValue)));
+               } else if (preference == maxFreqPref) {
+                   maxFreqPref.setSummary(String.format(MAX_FMT, MHerzed((String) newValue)));
+               }
+               return true;
+           } else {
+               return false;
+           }
         }
+        return false;
     }
 
-    private String ReadOneLine(String fname) {
+    public static String readOneLine(String fname) {
         BufferedReader br;
         String line = null;
 
@@ -154,28 +149,27 @@ public class CPUActivity extends PreferenceActivity implements Preference.OnPref
                 br.close();
             }
         } catch (Exception e) {
-            Log.e(LOGTAG, "IO Exception when reading /sys/ file", e);
+            Log.e(TAG, "IO Exception when reading /sys/ file", e);
         }
         return line;
     }
 
-    private void RootInvoker(String rootCommand) throws IOException {
-
-        Process process;
-        BufferedWriter stdin;
-
-        if (rootCommand != null) {
-            process = new ProcessBuilder("su").start();
-            stdin = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()), 512);
-            stdin.write(rootCommand);
-            stdin.close();
+    public static boolean writeOneLine(String fname, String value) {
+        try {
+            FileWriter fw = new FileWriter(fname);
             try {
-                process.waitFor();
-            } catch (InterruptedException e) {
-                Log.e(LOGTAG,"Interrupted on waitFor()", e);
+                fw.write(value);
+            } finally {
+                fw.close();
             }
+        } catch(IOException e) {
+            String Error = "Error writing to " + fname + ". Exception: ";
+            Log.e(TAG, Error, e);
+            return false;
         }
+        return true;
     }
+
 
     private String MHerzed(String str) {
         String temp;
