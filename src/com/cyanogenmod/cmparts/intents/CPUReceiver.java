5d440f66c43ac552335416fa6229f5b37571286b
Luca Santarelli <luca.santarelli@gmail.com>
2011-03-01 11:29:28 +0100

    CPUReceiver was missing from the previous commit.

--- src/com/cyanogenmod/cmparts/intents/CPUReceiver.java
+++ src/com/cyanogenmod/cmparts/intents/CPUReceiver.java
@@ -1,19 +1,22 @@
 package com.cyanogenmod.cmparts.intents;
 
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.os.SystemProperties;
+
+import com.cyanogenmod.cmparts.services.CPUService;
+import com.cyanogenmod.cmparts.services.ShutdownService;
 
 public class CPUReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context ctx, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            ComponentName cmp = new ComponentName(ctx.getPackageName(),
-                    com.cyanogenmod.cmparts.services.CPUService.class.getName());
-            ctx.startService(new Intent().setComponent(cmp));
+        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent _intent = new Intent(ctx, CPUService.class);
+            ctx.startService(_intent);
+        } else if (Intent.ACTION_SHUTDOWN.equals(intent.getAction())) {
+            Intent _intent = new Intent(ctx, ShutdownService.class);
+            ctx.startService(_intent);
         }
     }
 }
